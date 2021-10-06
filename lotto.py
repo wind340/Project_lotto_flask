81d282698e571433d035f805086a73c93623643a
Main <wind340@naver.com>
2021-10-06 16:15:39 +0900

    Update lotto.py

--- lotto.py
+++ lotto.py
@@ -13,16 +13,16 @@ def random_print() -> 'html':
     won = request.form['won']           #html에서 input된 won을 받아온다.
     title = '무작위 숫자 결과!'          #title에 str 담아서 return할때 the_title객체들에 다 뿌려준다.
     results = randomNum(int(won))       #won을 randomNum함수를 호출해서 results에 담아준다.
-    return render_template('results.html',      #return될 페이지 연결. title 지정한값, won 지정값, result결과물 
+    return render_template('results.html',      #return될 페이지 연결. html안에서 호출될 값을 담아준다. 
                            the_title=title,
                            the_won = won,
                            the_results=results
                            )
 
 @app.route('/')
-@app.route('/entry')
+@app.route('/entry')                    #연결이 되면 entry로 이동
 def entry_page() -> 'html':
-    return render_template('entry.html',
+    return render_template('entry.html',    #return될 페이지 연결. the_title이라는 변수에담아호출될 값을 정의해주었다
                            the_title='로또 번호 생성기!')
 
 if __name__ == '__main__':
